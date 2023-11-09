e87d18d4107501278521066d641d5a2711a4f0ea
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2023-11-09 15:34:37 +0300

    UI refactored

--- app/src/main/java/com/mertozan/koinsamples/ui/MainActivity.kt
+++ app/src/main/java/com/mertozan/koinsamples/ui/MainActivity.kt
@@ -1,36 +1,32 @@
 package com.mertozan.koinsamples.ui
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
-import com.mertozan.koinsamples.R
-import com.mertozan.koinsamples.core.data.dto.Person
+import androidx.appcompat.app.AppCompatActivity
 import com.mertozan.koinsamples.databinding.ActivityMainBinding
 import org.koin.android.ext.android.inject
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.component.KoinComponent
 
 class MainActivity : AppCompatActivity(), KoinComponent {
 
 
-    private val myViewModel : MainViewModel by inject()
-    private lateinit var binding : ActivityMainBinding
+    private val myViewModel: MainViewModel by inject()
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var personInfo = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         observeUi()
-
     }
 
-    private fun observeUi(){
-        myViewModel.personList.observe(this){ personList ->
-            binding.tvPerson.text = "List size: ${personList.size}"
+    private fun observeUi() {
+        myViewModel.personList.value.forEach { personList ->
+            personInfo += personList.name + " " + personList.age + " \n"
         }
+        binding.tvPerson.text = personInfo
     }
 }
--- app/src/main/java/com/mertozan/koinsamples/ui/MainViewModel.kt
+++ app/src/main/java/com/mertozan/koinsamples/ui/MainViewModel.kt
@@ -1,20 +1,20 @@
 package com.mertozan.koinsamples.ui
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mertozan.koinsamples.core.data.dto.Person
 import com.mertozan.koinsamples.core.domain.usecase.GetPersonDataUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 
 class MainViewModel(private val getPersonDataUseCase: GetPersonDataUseCase) :
     ViewModel(), KoinComponent {
 
-    private val _personList = MutableLiveData(emptyList<Person>())
-    val personList: LiveData<List<Person>> get() = _personList
+    private val _personList = MutableStateFlow(emptyList<Person>())
+    val personList: StateFlow<List<Person>> get() = _personList
 
     init {
         getAllPerson()
@@ -22,7 +22,7 @@ class MainViewModel(private val getPersonDataUseCase: GetPersonDataUseCase) :
 
     fun getAllPerson() {
         viewModelScope.launch {
-            _personList.postValue(getPersonDataUseCase())
+            _personList.value = getPersonDataUseCase()
             Log.e("Name", getPersonDataUseCase()[2].name)
         }
     }
